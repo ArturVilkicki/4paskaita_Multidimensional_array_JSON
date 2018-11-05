1e726203d13c4f869cb3c3e786c81b23e9c25773
ArturVilkicki <30698622+ArturVilkicki@users.noreply.github.com>
2018-11-05 16:10:46 +0200

    sasda

--- index2.php
+++ index2.php
@@ -7,32 +7,34 @@ $dataArray = json_decode($str, true);
 //print_r($jsonstr);
 // var_dump(json_decode($str));
 // var_dump($dataArray);
-
+/*
 foreach($dataArray as $mydata)
     {
-        // var_dump($mydata);
+        //var_dump($mydata);
         // echo $mydata->header . "\n"; 
 
          foreach($mydata as $item)
-         {
-            //is_array
+        {
+            
             if(is_array($item)){
                     foreach($item as $values)
                 {
-                    var_dump($values);
+                   var_dump($values);
                 }
-            }
+           }
             
          }
          
          
     }
+    */
 function funkcija($dataArray){
     if(is_array($dataArray)){
-       foreach ($item as $values) {
-            var_dump($values);
+       foreach ($dataArray as $items) {
+            var_dump($items);
         } 
     }
     
-}  
+}
+funkcija($dataArray);  
 
